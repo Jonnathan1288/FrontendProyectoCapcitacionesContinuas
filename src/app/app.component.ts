cb11cc99983308c60587cd0ebcb44c157c9fbedd
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-24 18:15:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoadScript } from './scripts/load-script';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FrontendProyectoCapcitacionesContinuas';
+   //public isLogginPresent: boolean = true;
+  public isLogginPresent: boolean = false;
+
+  constructor(private scriptC: LoadScript){
+    scriptC.Cargar(['dashboard']);
+  }
+  ngOnInit(): void {
+
+  }
 }
