d0ec4fbf40439788121e85ee55c81dcfd0c449ae
Sebas201964 <PEDRO.CABRERA.EST@TECAZUAY.EDU.EC>
2023-04-25 12:09:22 -0500

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { CurricularRegisterComponent } from './modules/curricular-diseño-register/curricular-diseño-register.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'diseño', component: CurricularRegisterComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
