9105ea21fb6ab32efa416536f7b69ed2964b21be
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-27 16:50:24 -0500

    new changes
    new changes

--- src/app/service/nivel-curso.service.ts
+++ src/app/service/nivel-curso.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/enviroment';
+import { Observable } from 'rxjs';
+import { NivelCurso } from '../models/nivel-curso';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NivelCursoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listNivelCurso():Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/nivelCurso/list');
+  }
+
+  public getNivelCursoById(idMivelCurso: number):Observable<NivelCurso>{
+    return this.http.get<NivelCurso>(environment.apiuri+'/nivelCurso/findbyId/'+idMivelCurso);
+  }
+
+  public saveNivelCurso(NivelCurso: NivelCurso):Observable<NivelCurso>{
+    return this.http.post<NivelCurso>(environment.apiuri+'/nivelCurso/save', NivelCurso);
+  }
+
+  public updateNivelCurso(idNivelCurso:number, NivelCurso: NivelCurso):Observable<NivelCurso>{
+    return this.http.put<NivelCurso>(environment.apiuri+'/nivelCurso/actualizar/'+idNivelCurso, NivelCurso);
+  }
+
 }
--- src/app/service/prerrequisitosCurso.service.ts
+++ src/app/service/prerrequisitosCurso.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/enviroment';
+import { Observable } from 'rxjs';
+import { PrerequisitoCurso } from '../models/prerequisito-curso';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrerrequisitosCursoService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  public listPrerequisitoCurso():Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/prerequisitoCurso/list');
+  }
+
+  public getPrerequisitoCursoById(idMivelCurso: number):Observable<PrerequisitoCurso>{
+    return this.http.get<PrerequisitoCurso>(environment.apiuri+'/prerequisitoCurso/findbyId/'+idMivelCurso);
+  }
+
+  public savePrerequisitoCurso(prerequisitoCurso: PrerequisitoCurso):Observable<PrerequisitoCurso>{
+    return this.http.post<PrerequisitoCurso>(environment.apiuri+'/prerequisitoCurso/save', prerequisitoCurso);
+  }
+
+  public updatePrerequisitoCurso(idPrerequisitoCurso:number, prerequisitoCurso: PrerequisitoCurso):Observable<PrerequisitoCurso>{
+    return this.http.put<PrerequisitoCurso>(environment.apiuri+'/prerequisitoCurso/actualizar/'+idPrerequisitoCurso, prerequisitoCurso);
+  }
 }
