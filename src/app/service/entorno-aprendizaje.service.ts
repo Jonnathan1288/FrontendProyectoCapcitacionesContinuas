91a5927d59073869295619f4ae77522ba78ec777
Sebas201964 <PEDRO.CABRERA.EST@TECAZUAY.EDU.EC>
2023-04-27 09:54:41 -0500

    v2

--- src/app/service/entorno-aprendizaje.service.ts
+++ src/app/service/entorno-aprendizaje.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/enviroment';
+import { Observable } from 'rxjs';
+import { EntornoAprendizajeCurricular} from '../models/entorno-aprendizaje-curricular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EntornoAprendizajeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listEntornoAprendizajeCurricular():Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/entornoAprendizajeCurricular/list');
+  }
+
+  public getEntornoAprendizajeCurricularById(idEntornoCurricular: number):Observable<EntornoAprendizajeCurricular>{
+    return this.http.get<EntornoAprendizajeCurricular>(environment.apiuri+'/entornoAprendizajeCurricular/findbyId/'+  idEntornoCurricular);
+  }
+
+  public saveEntornoAprendizajeCurricular(listEvaluacionFinalCurricular: EntornoAprendizajeCurricular):Observable<EntornoAprendizajeCurricular>{
+    return this.http.post<EntornoAprendizajeCurricular>(environment.apiuri+'/entornoAprendizajeCurricular/save',listEvaluacionFinalCurricular);
+  }
+
+  public updateEntornoAprendizajeCurricular(  idEntornoCurricular: number,entornoAprendizajeCurricular: EntornoAprendizajeCurricular): Observable<EntornoAprendizajeCurricular> {
+    return this.http.put<EntornoAprendizajeCurricular>(`${environment.apiuri}/entornoAprendizajeCurricular/update/${  idEntornoCurricular}`, entornoAprendizajeCurricular);
+  }
 }
--- src/app/service/evaluacion-diagnostico-curricular.service.ts
+++ src/app/service/evaluacion-diagnostico-curricular.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/enviroment';
+import { Observable } from 'rxjs';
+import { EvaluacionDiagnosticaCurriculares } from '../models/evaluacion-diagnostica-curriculares';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EvaluacionDiagnosticoCurricularService {
 
-  constructor() { }
+    
+  constructor(private http: HttpClient) { }
+
+  public listEvaluacionDiagnosticoCurricular():Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/evaluacionDiacnosticaCurricular/list');
+  }
+
+  public getEvaluacionDiagnosticoCurricularById(idEvaluacionDiagnosticaCurricular: number):Observable<EvaluacionDiagnosticaCurriculares>{
+    return this.http.get<EvaluacionDiagnosticaCurriculares>(environment.apiuri+'/evaluacionDiacnosticaCurricular/findbyId/'+ idEvaluacionDiagnosticaCurricular);
+  }
+
+  public saveEvaluacionDiagnosticoCurricular(listDisenioCurricular: EvaluacionDiagnosticaCurriculares):Observable<EvaluacionDiagnosticaCurriculares>{
+    return this.http.post<EvaluacionDiagnosticaCurriculares>(environment.apiuri+'/evaluacionDiacnosticaCurricular/save',listDisenioCurricular);
+  }
+
+  public updateEvaluacionDiagnosticoCurricular( idEvaluacionDiagnosticaCurricular: number, evaluacionDiacnosticaCurricular: EvaluacionDiagnosticaCurriculares): Observable<EvaluacionDiagnosticaCurriculares> {
+    return this.http.put<EvaluacionDiagnosticaCurriculares>(`${environment.apiuri}/evaluacionDiacnosticaCurricular/update/${ idEvaluacionDiagnosticaCurricular}`, evaluacionDiacnosticaCurricular);
+  }
 }
