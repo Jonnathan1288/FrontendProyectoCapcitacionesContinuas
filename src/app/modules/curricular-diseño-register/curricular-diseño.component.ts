693bcdec0a80b0aae56c5b9e403d5c4c26f31c5f
Sebas201964 <PEDRO.CABRERA.EST@TECAZUAY.EDU.EC>
2023-04-28 11:51:43 -0500

    v2

--- src/app/modules/curricular-diseño-register/curricular-diseño.component.ts
+++ src/app/modules/curricular-diseño-register/curricular-diseño.component.ts
@@ -3,11 +3,13 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { LoadScript } from 'src/app/scripts/load-script';
 import { Silabo } from 'src/app/models/silabo';
 import { EvaluacionDiagnosticaCurriculares } from 'src/app/models/evaluacion-diagnostica-curriculares';
+import { EvalucionFormativaCurriculares } from 'src/app/models/evalucion-formativa-curriculares';
 import { EvaluacionFinalCurriculares } from 'src/app/models/evaluacion-final-curriculares';
-import { EntornoAprendizajeCurricular} from 'src/app/models/entorno-aprendizaje-curricular';
+import { EntornoAprendizajeCurricular } from 'src/app/models/entorno-aprendizaje-curricular';
 import { SilaboService } from 'src/app/service/silabo.service';
 import { DisenioCurricularService } from 'src/app/service/disenio-curricular.service';
 import { EvaluacionDiagnosticoCurricularService } from 'src/app/service/evaluacion-diagnostico-curricular.service';
+import { EvaluacionFormativaCurricularService } from 'src/app/service/evaluacion-formativa-curricular.service';
 import { EvaluacionFinalCurricularService } from 'src/app/service/evaluacion-final-curricular.service';
 import { EntornoAprendizajeService } from 'src/app/service/entorno-aprendizaje.service';
 import { Router } from '@angular/router';
@@ -23,19 +25,21 @@ export class CurricularDiseñoComponent implements OnInit {
     private router: Router,
     private silaboService: SilaboService,
     private disenioCurricularService: DisenioCurricularService,
-    private evaluacionDiagnosticoCurricularService:EvaluacionDiagnosticoCurricularService,
+    private evaluacionDiagnosticoCurricularService: EvaluacionDiagnosticoCurricularService,
+    private evaluacionFormativaCurricularService: EvaluacionFormativaCurricularService,
     private evaluacionFinalCurricularService: EvaluacionFinalCurricularService,
     private entornoAprendizajeService: EntornoAprendizajeService
-    
+
   ) {
   }
 
   /* ENTIDADES UTILIZAR*/
   silabo: Silabo = new Silabo();
   disenioCurricular: DisenioCurriculares = new DisenioCurriculares();
-  evaluacionDiagnosticoCurricular:EvaluacionDiagnosticaCurriculares= new EvaluacionDiagnosticaCurriculares();
-  evaluacionFinalCurricular:EvaluacionFinalCurriculares= new EvaluacionFinalCurriculares();
-  entornoAprendizajeCurricular:EntornoAprendizajeCurricular= new EntornoAprendizajeCurricular();
+  evaluacionDiagnosticoCurricular: EvaluacionDiagnosticaCurriculares = new EvaluacionDiagnosticaCurriculares();
+  evaluacionFormativaCurricular: EvalucionFormativaCurriculares = new EvalucionFormativaCurriculares();
+  evaluacionFinalCurricular: EvaluacionFinalCurriculares = new EvaluacionFinalCurriculares();
+  entornoAprendizajeCurricular: EntornoAprendizajeCurricular = new EntornoAprendizajeCurricular();
 
 
 
@@ -61,7 +65,7 @@ export class CurricularDiseñoComponent implements OnInit {
   /* FIN TRAER DATOS*/
 
   /* CREACION RESULTADOS EVALUACION DIAGNOSTICA- ARRAY TEMPORAL*/
-  listEvaluacionDiagnosticaCurricular:EvaluacionDiagnosticaCurriculares[] = [];
+  listEvaluacionDiagnosticaCurricular: EvaluacionDiagnosticaCurriculares[] = [];
 
 
   public almacenarListaEvaluacionD(): void {
@@ -81,24 +85,72 @@ export class CurricularDiseñoComponent implements OnInit {
 
   /* FIN RESULTADOS Evaluacion Diagnostica */
 
-   /* CREACION RESULTADOS EVALUACION FINAL- ARRAY TEMPORAL*/
-   listEvaluacionFinalCurricular:EvaluacionFinalCurriculares[] = [];
+
+  /* CREACION RESULTADOS EVALUACION FORMATIVA- ARRAY TEMPORAL*/
+  listEvaluacionFormativaCurricular: EvalucionFormativaCurriculares[] = [];
 
 
-   public almacenarListaEvaluacionF(): void {
-     if (!this.evaluacionFinalCurricular.instrumnetoFormativaFinal || !this.evaluacionFinalCurricular.tecnicaFormativaFinal) {
-       alert("Campos Vacios")
-     } else {
-       this.listEvaluacionFinalCurricular.push(this.evaluacionFinalCurricular);
-       this.evaluacionFinalCurricular = new EvaluacionFinalCurriculares();
-       this.evaluacionFinalCurricular.instrumnetoFormativaFinal = '';
-       this.evaluacionFinalCurricular.tecnicaFormativaFinal = '';
-     }
-   }
- 
-   public quitarElementoEvaluacionF(index: number): void {
-     this.listEvaluacionFinalCurricular.splice(index, 1);
-   }
- 
-   /* FIN RESULTADOS Evaluacion Diagnostica */
+  public almacenarListaEvaluacionFormativa(): void {
+    if (!this.evaluacionFormativaCurricular.instrumnetoFormativa || !this.evaluacionFormativaCurricular.tecnicaFormativa) {
+      alert("Campos Vacios")
+    } else {
+      this.listEvaluacionFormativaCurricular.push(this.evaluacionFormativaCurricular);
+      this.evaluacionFormativaCurricular = new EvalucionFormativaCurriculares();
+      this.evaluacionFormativaCurricular.instrumnetoFormativa = '';
+      this.evaluacionFormativaCurricular.tecnicaFormativa = '';
+    }
+  }
+
+  public quitarElementoEvaluacionFormativa(index: number): void {
+    this.listEvaluacionFormativaCurricular.splice(index, 1);
+  }
+
+  /* FIN RESULTADOS Evaluacion FORMATIVA */
+
+
+  /* CREACION RESULTADOS EVALUACION FINAL- ARRAY TEMPORAL*/
+  listEvaluacionFinalCurricular: EvaluacionFinalCurriculares[] = [];
+
+
+  public almacenarListaEvaluacionF(): void {
+    if (!this.evaluacionFinalCurricular.instrumnetoFormativaFinal || !this.evaluacionFinalCurricular.tecnicaFormativaFinal) {
+      alert("Campos Vacios")
+    } else {
+      this.listEvaluacionFinalCurricular.push(this.evaluacionFinalCurricular);
+      this.evaluacionFinalCurricular = new EvaluacionFinalCurriculares();
+      this.evaluacionFinalCurricular.instrumnetoFormativaFinal = '';
+      this.evaluacionFinalCurricular.tecnicaFormativaFinal = '';
+    }
+  }
+
+  public quitarElementoEvaluacionF(index: number): void {
+    this.listEvaluacionFinalCurricular.splice(index, 1);
+  }
+
+  /* FIN RESULTADOS Evaluacion FINAL */
+
+
+  /* CREACION RESULTADOS ENTORNO APRENDIZAJE- ARRAY TEMPORAL*/
+  listEntornoAprendizaje: EntornoAprendizajeCurricular[] = [];
+
+
+  public almacenarEntornoAprendizaje(): void {
+    if (!this.entornoAprendizajeCurricular.instalaciones || !this.entornoAprendizajeCurricular.faseTeorica
+      || !this.entornoAprendizajeCurricular.fasePractica) {
+      alert("Campos Vacios")
+    } else {
+      this.listEntornoAprendizaje.push(this.entornoAprendizajeCurricular);
+      this.entornoAprendizajeCurricular = new EntornoAprendizajeCurricular();
+      this.entornoAprendizajeCurricular.instalaciones = '';
+      this.entornoAprendizajeCurricular.faseTeorica = '';
+      this.entornoAprendizajeCurricular.fasePractica = '';
+    }
+  }
+
+  public quitarEntornoAprendizaje(index: number): void {
+    this.listEntornoAprendizaje.splice(index, 1);
+  }
+
+  /* FIN RESULTADOS ENTORNO APRENDIZAJE */
 }
+
