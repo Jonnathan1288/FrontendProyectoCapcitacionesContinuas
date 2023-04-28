b7508ea52d991070cccde3499cb719020f4db78e
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-28 15:35:39 -0500

    New chnages
    New chnages

--- src/app/service/programas.service.ts
+++ src/app/service/programas.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environment/enviroment';
 import { Observable } from 'rxjs';
-import { Programa } from '../models/programa';
+import { Programas } from '../models/programa';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +15,15 @@ export class ProgramasService {
     return this.http.get<any>(environment.apiuri+'/programa/listar');
   }
 
-  public getProgramaById(idPrograma: number):Observable<Programa>{
-    return this.http.get<Programa>(environment.apiuri+'/programa/findbyId/'+idPrograma);
+  public getProgramaById(idPrograma: number):Observable<Programas>{
+    return this.http.get<Programas>(environment.apiuri+'/programa/findbyId/'+idPrograma);
   }
 
-  public savePrograma(programa: Programa):Observable<Programa>{
-    return this.http.post<Programa>(environment.apiuri+'/programa/crear', programa);
+  public savePrograma(programa: Programas):Observable<Programas>{
+    return this.http.post<Programas>(environment.apiuri+'/programa/crear', programa);
   }
 
-  public updatePrograma(idPrograma:number, programa: Programa):Observable<Programa>{
-    return this.http.put<Programa>(environment.apiuri+'/programa/actualizar/'+idPrograma, programa);
+  public updatePrograma(idPrograma:number, programa: Programas):Observable<Programas>{
+    return this.http.put<Programas>(environment.apiuri+'/programa/actualizar/'+idPrograma, programa);
   }
 }
