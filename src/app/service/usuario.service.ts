6700e5c59a85cc7ee899aec9e79cc2002e1a57a3
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-29 15:45:18 -0500

    nuevos cambios inscripcion
    nuevos cambios inscripcion

--- src/app/service/usuario.service.ts
+++ src/app/service/usuario.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environment/enviroment';
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public listUsuario():Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/usuario/listar');
+  }
+
+  public getUsuarioById(idUsuario: number):Observable<Usuario>{
+    return this.http.get<Usuario>(environment.apiuri+'/usuario/findbyId/'+idUsuario);
+  }
+
+  public saveUsuario(usuario: Usuario):Observable<Usuario>{
+    return this.http.post<Usuario>(environment.apiuri+'/usuario/crear', usuario);
+  }
+
+  public updateUsuario(idUsusario:number, usuario: Usuario):Observable<Usuario>{
+    return this.http.put<Usuario>(environment.apiuri+'/usuario/actualizar/'+idUsusario, usuario);
+  }
 }
