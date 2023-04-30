d82fc0eec552209e14c7a3854ee25f6bffe90323
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-04-30 02:21:35 -0500

    otros cambios
    otros cambios

--- src/app/modules/view-inscritos-curso/tomar-asistencia-estudiante/tomar-asistencia-estudiante.component.ts
+++ src/app/modules/view-inscritos-curso/tomar-asistencia-estudiante/tomar-asistencia-estudiante.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Asistencia } from 'src/app/models/asistencia';
+import { AsistenciaService } from 'src/app/service/asistencia.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-tomar-asistencia-estudiante',
@@ -8,17 +10,63 @@ import { Asistencia } from 'src/app/models/asistencia';
   styleUrls: ['./tomar-asistencia-estudiante.component.css'],
 })
 export class TomarAsistenciaEstudianteComponent implements OnInit {
+  today = new Date();
+  //La clase para la asistencia
   public asistenciaEstudiante = new Asistencia();
+
   //Para capturar el id de los matriculados
-  public idEstudiantesMatriculados?: number;
-  constructor(private activateRoute: ActivatedRoute, private router: Router) {}
+  public idCursoEstudiantesMatriculados?: number;
+
+//el array de los estudiantes del curso matriculado
+public listaAsistenciaE: any[] =[];
+
+  constructor(private activateRoute: ActivatedRoute, private router: Router,
+    private asistenciaService:AsistenciaService) {}
   ngOnInit(): void {
     this.activateRoute.params.subscribe((param) => {
       const idEstudiantesM = param['id'];
-      this.idEstudiantesMatriculados = idEstudiantesM;
-      // this.traerParticipantesMatriculados(this.idCursoMatricula!);
+      this.idCursoEstudiantesMatriculados = idEstudiantesM;
+      this.traerListadoEstudiantesMatriculadosAsistencia(this.idCursoEstudiantesMatriculados!);
     });
+  }
+  
+
+  public traerListadoEstudiantesMatriculadosAsistencia(idCurso: number){
+    this.asistenciaService.generarAsistenciaPorFecha(idCurso).subscribe((data)=>{
+      if(data != null){
+        this.listaAsistenciaE = data;
+      }
+    })
+  }
+
+  public tomarAsistenciaCursoEstudiante(idAsist: any, asistencia: Asistencia){
+
+    this.asistenciaService.updateAsistencia(idAsist, asistencia).subscribe((data)=>{
+      if(data != null){
+        console.log(data)
+      
+        alert('Tomado lista')
+      }
+    })
+  }
+
+  public tomarAsistenciaCursoEstudianteCP(){
+
+    this.asistenciaService.updateAsistencia(this.asistenciaEstudiante.idAsistencia!, this.asistenciaEstudiante).subscribe((data)=>{
+      if(data != null){
+    
+      
+        alert('Tomado lista')
+      }
+    })
+    this.visible = false
   }
 
 
+  visible: boolean = false;
+
+  showDialog(asistencia: Asistencia) {
+    this.asistenciaEstudiante = { ...asistencia };
+    this.visible = true;
+  }
 }
--- src/app/service/asistencia.service.ts
+++ src/app/service/asistencia.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Curso } from '../models/curso';
+import { environment } from 'src/environment/enviroment';
+import { Observable } from 'rxjs';
+import { Area } from '../models/area';
+import { Asistencia } from '../models/asistencia';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AsistenciaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public generarAsistenciaPorFecha(idCurso: number):Observable<any>{
+    return this.http.get<any>(environment.apiuri+'/asistencia/GenerarAsistencia/'+idCurso);
+  }
+
+  public getAreaById(idAsistencia: number):Observable<Asistencia>{
+    return this.http.get<Asistencia>(environment.apiuri+'/asistencia/findbyId/'+idAsistencia);
+  }
+
+  public saveAsistencia(asistencia: Asistencia):Observable<Asistencia>{
+    return this.http.post<Asistencia>(environment.apiuri+'/asistencia/save', asistencia);
+  }
+
+  public updateAsistencia(idAsistencia: number, asistencia: Asistencia):Observable<Asistencia>{
+    return this.http.put<Asistencia>(environment.apiuri+'/asistencia/actualizar/'+idAsistencia, asistencia);
+  }
 }
