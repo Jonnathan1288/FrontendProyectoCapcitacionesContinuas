ac01ce46dca70f1a565305251cb8d82f5451352f
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-05-02 11:32:19 -0500

    create docente capacitador
    create docente capacitador

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -1,6 +1,8 @@
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
+import { Persona } from '../models/persona';
+import { environment } from 'src/environment/enviroment';
 
 
 @Injectable({
@@ -8,6 +10,22 @@ import { Observable, map } from 'rxjs';
 })
 export class PersonaService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  public getListaPersonas():Observable<Persona[]>{
+    return this.http.get<Persona[]>(environment.apiuri+'/persona/listar');
+  }
+
+  public savePersona(persona: Persona):Observable<Persona>{
+    return this.http.post<Persona>(environment.apiuri+'/persona/crear', persona);
+  }
+
+  public getPersonaById(idPersona: number):Observable<Persona>{
+    return this.http.get<Persona>(environment.apiuri+'/persona/findbyId/'+idPersona);
+  }
+
+  public updatePersona(idPersona:number, Persona: Persona):Observable<Persona>{
+    return this.http.put<Persona>(environment.apiuri+'/persona/actualizar/'+idPersona, Persona);
+  }
 
 }
