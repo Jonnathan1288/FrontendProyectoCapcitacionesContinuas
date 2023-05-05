bafd8932bb94a983c9a753fb4da51d0f09e5855e
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-05-05 15:38:59 -0500

    n
    n

--- src/app/modules/view-inscritos-curso/registrar-notas-finales/registrar-notas-finales.component.ts
+++ src/app/modules/view-inscritos-curso/registrar-notas-finales/registrar-notas-finales.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Curso } from 'src/app/models/curso';
+import { InformeFinalCurso } from 'src/app/models/informe-final-curso';
 import { Notas } from 'src/app/models/notas';
 import { ParticipantesMatriculados } from 'src/app/models/participantesMatriculados';
 import { CursoService } from 'src/app/service/curso.service';
+import { InformeFinalCursoService } from 'src/app/service/informe-final-curso.service';
 import { NotasService } from 'src/app/service/notas.service';
 import { ParticipanteMatriculadoService } from 'src/app/service/participante-matriculado.service';
 import { ReportsCapacitacionesService } from 'src/app/service/reports-capacitaciones.service';
@@ -10,91 +13,88 @@ import { ReportsCapacitacionesService } from 'src/app/service/reports-capacitaci
 @Component({
   selector: 'app-registrar-notas-finales',
   templateUrl: './registrar-notas-finales.component.html',
-  styleUrls: ['./registrar-notas-finales.component.css']
+  styleUrls: ['./registrar-notas-finales.component.css'],
 })
-export class RegistrarNotasFinalesComponent implements OnInit{
-
+export class RegistrarNotasFinalesComponent implements OnInit {
   constructor(
     private activateRoute: ActivatedRoute,
     private router: Router,
     private participantesMatriculadosService: ParticipanteMatriculadoService,
     private notasService: NotasService,
-    private resportService: ReportsCapacitacionesService
+    private resportService: ReportsCapacitacionesService,
+    private informeFinalCorseService: InformeFinalCursoService,
+    private cursoService: CursoService
   ) {}
 
   idCursoGlobal?: number;
 
-
   ngOnInit(): void {
-    this.activateRoute.params.subscribe( (param) =>{
+    this.activateRoute.params.subscribe((param) => {
       const idCursoRout = param['id'];
-      console.log("Idcurso => " + idCursoRout)
+      console.log('Idcurso => ' + idCursoRout);
       this.idCursoGlobal = idCursoRout;
       this.validarExistenciaDeRegistros();
     });
   }
 
   isValidateExistenciaNotas!: boolean;
-  public validarExistenciaDeRegistros():void{
-    this.notasService.validarExistenciaDatos(this.idCursoGlobal!).subscribe(
-      data=>{
+  public validarExistenciaDeRegistros(): void {
+    this.notasService
+      .validarExistenciaDatos(this.idCursoGlobal!)
+      .subscribe((data) => {
         if (data == false) {
           // SI HAY DATOS
-          alert("si hay")
+          alert('si hay');
           this.isValidateExistenciaNotas = false;
           this.obtenerParticipantesFinales();
         } else {
           // NO HAY DATOS
-          alert("no hay")
+          alert('no hay');
           this.isValidateExistenciaNotas = true;
           this.traerParticipantesMatriculados();
         }
-      }
-    )
+      });
   }
 
-  listaParticipantesMatriculados: ParticipantesMatriculados [] = [];
+  listaParticipantesMatriculados: ParticipantesMatriculados[] = [];
   // TRAER TODOS LOS MATRICULADOS DEL CURSO //
-  public traerParticipantesMatriculados():void{
-    this.participantesMatriculadosService.getParticipantesMatriculadosByIdCurso(this.idCursoGlobal!).subscribe(
-      data => {
+  public traerParticipantesMatriculados(): void {
+    this.participantesMatriculadosService
+      .getParticipantesMatriculadosByIdCurso(this.idCursoGlobal!)
+      .subscribe((data) => {
         this.listaParticipantesMatriculados = data;
-        console.log("trayendo -> " + this.listaParticipantesMatriculados)
+        console.log('trayendo -> ' + this.listaParticipantesMatriculados);
         this.guardarDatosVacios();
-      }
-    )
+      });
   }
   // FIN //
   notas: Notas = new Notas();
   // MANDAR LOS DATOS A LA TABLA
-  public guardarDatosVacios():void{
+  public guardarDatosVacios(): void {
     for (let participante of this.listaParticipantesMatriculados) {
       const notas = new Notas();
       notas.partipantesMatriculados = participante;
       notas.examenFinal = 0;
-      notas.observacion = "";
+      notas.observacion = '';
       notas.parcial = 0;
-      this.notasService.saveNotas(notas).subscribe(
-        data=>{
-          alert("se registró el participante ");
-          this.obtenerParticipantesFinales();
-        }
-      )
+      this.notasService.saveNotas(notas).subscribe((data) => {
+        alert('se registró el participante ');
+        this.obtenerParticipantesFinales();
+      });
     }
   }
   //
 
-  // TAER TODOS LOS ESTUDIANTES YA GUARDADOS 
+  // TAER TODOS LOS ESTUDIANTES YA GUARDADOS
   listNotas: Notas[] = [];
 
-  public obtenerParticipantesFinales():void{
-    this.notasService.getParticipantesFinales(this.idCursoGlobal!).subscribe(
-      data =>{
+  public obtenerParticipantesFinales(): void {
+    this.notasService
+      .getParticipantesFinales(this.idCursoGlobal!)
+      .subscribe((data) => {
         this.listNotas = data;
-      }
-    )
+      });
   }
-  
 
   // CREAR NOTAS POR ESTUDIANTE
 
@@ -102,71 +102,127 @@ export class RegistrarNotasFinalesComponent implements OnInit{
   visible?: boolean;
   idCapModelEdit?: number;
 
-
   // EDIT AND CREATE RESULTADOS //
   public showModalNotas(idParticpanteNota: number) {
     this.visible = true;
-      this.notasService.getNotasById(idParticpanteNota).subscribe(
-        data => {
-          this.notas = data;
-          this.idCapModelEdit = this.notas.idNota;
-        }
-      )
+    this.notasService.getNotasById(idParticpanteNota).subscribe((data) => {
+      this.notas = data;
+      this.idCapModelEdit = this.notas.idNota;
+    });
   }
-  
 
-  public guardarNotaPorEstudiante():void{
-    this.notasService.updateNotas(this.idCapModelEdit!,this.notas).subscribe(
-      data=>{
-        this.notas= data;
+  public guardarNotaPorEstudiante(): void {
+    this.notasService
+      .updateNotas(this.idCapModelEdit!, this.notas)
+      .subscribe((data) => {
+        this.notas = data;
         this.obtenerParticipantesFinales();
-        alert("se registro la nota")
+        alert('se registro la nota');
         this.visible = false;
-      }
-    )       
+      });
   }
   //
 
-
-  // VALIDAR LAS NOTAS FINALES 
+  // VALIDAR LAS NOTAS FINALES
   // A:APROBADOS R:REPROBADOS X:RETIRADOS
-  participantesMatriculado: ParticipantesMatriculados = new ParticipantesMatriculados();
-  idParticpanteNota!:number;
-  public vaidarNotasEstudiantesFinales():void{
-    console.log("CLICk")
+  participantesMatriculado: ParticipantesMatriculados =
+    new ParticipantesMatriculados();
+  idParticpanteNota!: number;
+  public vaidarNotasEstudiantesFinales(): void {
+    console.log('CLICk');
     for (let participante of this.listNotas) {
       const parcial = participante.parcial!;
       const examen = participante.examenFinal!;
-      this.idParticpanteNota = participante.partipantesMatriculados!.idParticipanteMatriculado!;
-      const notaFinal = (parcial * 0.40) + (examen * 0.60);
-      console.log(" Esta es su nota final -> " + notaFinal )
+      this.idParticpanteNota =
+        participante.partipantesMatriculados!.idParticipanteMatriculado!;
+      const notaFinal = parcial * 0.4 + examen * 0.6;
+      console.log(' Esta es su nota final -> ' + notaFinal);
 
-      this.participantesMatriculadosService.getParticipantesMatriculadosById(this.idParticpanteNota).subscribe(
-        data =>{
+      this.participantesMatriculadosService
+        .getParticipantesMatriculadosById(this.idParticpanteNota)
+        .subscribe((data) => {
           this.participantesMatriculado = data;
           if (notaFinal >= 7) {
-            console.log("APROBADO");
-            this.participantesMatriculado.estadoParticipanteAprobacion = "A";
+            console.log('APROBADO');
+            this.participantesMatriculado.estadoParticipanteAprobacion = 'A';
           } else {
-            console.log("NO APROBADO")
-            this.participantesMatriculado.estadoParticipanteAprobacion = "R";
+            console.log('NO APROBADO');
+            this.participantesMatriculado.estadoParticipanteAprobacion = 'R';
           }
-          this.participantesMatriculadosService.updateParticipantesMatriculados(this.idParticpanteNota, this.participantesMatriculado).subscribe(
-            data =>{
-              console.log("Se actulizo su estado APROBACION")
-            }
-          )
-        }
-      )
-      
+          this.participantesMatriculadosService
+            .updateParticipantesMatriculados(
+              this.idParticpanteNota,
+              this.participantesMatriculado
+            )
+            .subscribe((data) => {
+              console.log('Se actulizo su estado APROBACION');
+            });
+        });
     }
   }
 
-  public generarReporteAsitenciaEvaluacion():void{
-    this.resportService.downloadAsistenciaEvaluacion(this.idCursoGlobal!)
+  public generarReporteAsitenciaEvaluacion(): void {
+    this.resportService
+      .downloadAsistenciaEvaluacion(this.idCursoGlobal!)
       .subscribe((r) => {
         const url = URL.createObjectURL(r);
         window.open(url, '_blank');
       });
+  }
+
+  //IMPLEMENTACION PARA QUE LLENE EL FORMULARIO FINAL DE CURSO..
+
+  visibleModalFormFinalCourse?: boolean= false;
+
+  // EDIT AND CREATE RESULTADOS //
+  public classInformeFinalC = new InformeFinalCurso();
+
+  public showModalFinalInformationCorse() {
+    this.informeFinalCorseService
+      .getInformeFinalCursoByIdCurso(this.idCursoGlobal!)
+      .subscribe(
+        (data) => {
+          if (data != null) {
+            this.classInformeFinalC = data;
+            this.classInformeFinalC.idInformeFinalCurso = data.idInformeFinalCurso
+          }
+        },
+        (err) => {
+          alert('ee');
+        }
+      );
+    this.visibleModalFormFinalCourse = true;
+  }
+
+  public saveUpdateInformFinalCourse() {
+    this.classInformeFinalC = { ...this.classInformeFinalC };
+    if (this.classInformeFinalC.idInformeFinalCurso) {
+      
+      this.informeFinalCorseService.updateInformeFinalCurso(this.classInformeFinalC.idInformeFinalCurso, this.classInformeFinalC).subscribe((data)=>{
+        if(data != null){
+          alert('succesful update')
+          this.visibleModalFormFinalCourse = false;
+        }
+      })
+    } else {
+      this.cursoService.getCursoById(this.idCursoGlobal!).subscribe(
+        (data) => {
+          if (data != null) {
+            this.classInformeFinalC.curso = data;
+            this.informeFinalCorseService
+              .saveInformeFinalCurso(this.classInformeFinalC)
+              .subscribe((data) => {
+                if (data != null) {
+                  alert('succesful');
+                  console.log({ cf: data });
+                }
+              });
+          }
+        },
+        (err) => {
+          alert('exption' + err);
+        }
+      );
+    }
   }
 }
