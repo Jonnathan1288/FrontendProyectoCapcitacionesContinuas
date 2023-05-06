999ee826319736c4727e730096aeab07dae3e031
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-05-06 09:14:24 -0500

    n
    n

---
+++ src/app/models/canton.ts
@@ -0,0 +1,7 @@
+import { Provincia } from './provincia';
+
+export class Canton {
+  idCanton?: number;
+  canton?: string;
+  provincia?: Provincia;
+}
--- src/app/models/curso.ts
+++ src/app/models/curso.ts
@@ -1,31 +1,34 @@
-import { Capacitador } from "./capacitador";
-import { Especialidad } from "./especialidad";
-import { HorarioCurso } from "./horario-curso";
-import { ModalidadCurso } from "./modalidad-curso";
-import { NivelCurso } from "./nivel-curso";
-import { Programas } from "./programa";
-import { TipoCurso } from "./tipo-curso";
+import { Capacitador } from './capacitador';
+import { Especialidad } from './especialidad';
+import { HorarioCurso } from './horario-curso';
+import { ModalidadCurso } from './modalidad-curso';
+import { NivelCurso } from './nivel-curso';
+import { Parroquia } from './parroquia';
+import { Programas } from './programa';
+import { TipoCurso } from './tipo-curso';
 
 export class Curso {
-    idCurso?: number;
-    nombreCurso?: string; //
-    fotoCurso?: string; //no
-    duracionCurso?: number; //
-    observacionCurso?: string; //
-    estadoCurso?: boolean;
-    estadoAprovacionCurso?: string;
-    estadoPublicasionCurso?: string;
-    descripcionCurso?: string;
-    objetivoGeneralesCurso?: string;
-    numeroCuposCurso?: number;
-    fechaInicioCurso?: Date;
-    fechaFinalizacionCurso?: Date;
-    programas?: Programas;
-    especialidad?: Especialidad;
-    capacitador?: Capacitador;
-    modalidadCurso?: ModalidadCurso;
-    tipoCurso?: TipoCurso
-    nivelCurso?: NivelCurso
-    horarioCurso?: HorarioCurso;
-    iniciocurso?: boolean
+  idCurso?: number;
+  nombreCurso?: string; //
+  fotoCurso?: string; //no
+  duracionCurso?: number; //
+  observacionCurso?: string; //
+  estadoCurso?: boolean;
+  estadoAprovacionCurso?: string;
+  estadoPublicasionCurso?: string;
+  descripcionCurso?: string;
+  objetivoGeneralesCurso?: string;
+  numeroCuposCurso?: number;
+  fechaInicioCurso?: Date;
+  fechaFinalizacionCurso?: Date;
+  programas?: Programas;
+  cursoocc?: string;
+  especialidad?: Especialidad;
+  capacitador?: Capacitador;
+  modalidadCurso?: ModalidadCurso;
+  tipoCurso?: TipoCurso;
+  nivelCurso?: NivelCurso;
+  horarioCurso?: HorarioCurso;
+  parroquia?: Parroquia;
+  // iniciocurso?: boolean
 }
