73578fbe0a5c7e784da9d38182deb29ae8853dfe
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-08-22 23:20:47 -0500

    final
    final

--- src/app/models/notas.ts
+++ src/app/models/notas.ts
@@ -3,6 +3,9 @@ import { ParticipantesMatriculados } from "./participantesMatriculados";
 export class Notas {
     idNota?: number;
     parcial?: number;
+    informe1?: number;
+    informe2?: number;
+    informe3?: number;
     examenFinal?: number;
     fechaNota?: Date;
     observacion?: string;
