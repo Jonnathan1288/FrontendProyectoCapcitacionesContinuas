27bf6cab206cf8b3d198dcb3a632f13377a9296b
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-10-04 21:44:56 -0500

    profile

--- src/app/modules/oauthModule/edit-data-user/edit-data-user.component.ts
+++ src/app/modules/oauthModule/edit-data-user/edit-data-user.component.ts
@@ -20,17 +20,16 @@ import { UploadService } from 'src/app/service/upload.service';
 	styleUrls: ['./edit-data-user.component.css'],
 })
 export class EditDataUserComponent implements OnInit {
+
 	public classPersona = new Persona();
-
+	public persona = new Persona();
 	public classUsuario = new Usuario();
-
 	public classCapacitador = new Capacitador();
-
 	public idUsuario?: any;
-
 	public usuLoginRol?: any;
-
 	public esDocenteCapacitador = false;
+	public urlPhoto: any = '';
+	public classCopyCapacitador = new Capacitador();
 
 	constructor(
 		private route: ActivatedRoute,
@@ -42,51 +41,31 @@ export class EditDataUserComponent implements OnInit {
 		private toastrService: ToastrService,
 		private scriptC: LoadScript,
 		private imagenService: UploadService
-	) {
-
-		scriptC.Cargar(['ventanas']);
-
-	}
-
-	public persona = new Persona();
-
-
-	//------------------------------------------
-	public urlPhoto: any = '';
+	) {}
 
 	ngOnInit(): void {
 		this.idUsuario = localStorage.getItem('id_username');
 		this.usuLoginRol = localStorage.getItem('rol');
 		this.obtenerDatosUsusario(this.idUsuario);
-
 		if (localStorage.getItem('emp') === 'EMPTY') {
 			this.toastrService.info('', 'COMPLETE SU PERFIL DE USUARIO');
 		}
-
 		this.urlPhoto = getAttributeStorage(LocalStorageKeys.URL_PHOTO);
-
 	}
 
-
-
-	public classCipyCapacitador = new Capacitador();
+	
 	public obtenerDatosUsusario(idUsusario: number) {
 		this.usuarioService.getUsuarioById(idUsusario).subscribe((data) => {
 			if (data != null) {
 				this.classUsuario = data;
 				this.persona = this.classUsuario.persona!;
-
 				if (this.persona.fechaNacimiento) {
+					// PARSE THE DATE
 					this.persona.fechaNacimiento = new Date(
 						this.persona?.fechaNacimiento
-					); //
+					); 
 				}
-
-				this.persona.etnia =
-					this.persona?.etnia!.charAt(0).toUpperCase() +
-					this.persona?.etnia!.slice(1).toLowerCase();
-
-				this.classPersona = { ...this.persona }; ///
+				this.classPersona = { ...this.persona }; 
 
 				if (this.usuLoginRol === 'DocenteCapacitador') {
 					this.capacitadorService
@@ -95,239 +74,14 @@ export class EditDataUserComponent implements OnInit {
 						)
 						.subscribe((data) => {
 							if (data != null) {
-								this.classCipyCapacitador = data;
-								this.classCapacitador = { ...this.classCipyCapacitador };
+								this.classCopyCapacitador = data;
+								this.classCapacitador = { ...this.classCopyCapacitador };
 							}
 						});
 				}
 			}
 		});
 	}
-
-	//VISIVILIADA DEL MODAL
-	visible?: boolean;
-
-	public showModaL() {
-		this.classPersona = { ...this.persona };
-		this.classCapacitador = { ...this.classCipyCapacitador };
-		console.log(this.classPersona);
-		console.log(this.persona);
-		this.visible = true;
-	}
-
-	reloadPage() {
-		this.classPersona = { ...this.persona };
-	}
-
-	public editarUsuario() {
-		if (this.usuLoginRol === 'DocenteCapacitador') {
-			if (
-				!this.classUsuario.username ||
-				!this.classUsuario.password
-			) {
-				this.toastrService.warning(
-					'Uno o más campos vacíos',
-					'Por favor complete todos los campos'
-				);
-				return;
-			}
-		} else {
-			if (
-				!this.classUsuario.username ||
-				!this.classUsuario.password
-			) {
-				this.toastrService.warning(
-					'Uno o más campos vacíos',
-					'Por favor complete todos los campos'
-				);
-				return;
-			}
-		}
-		this.personaService
-			.updatePersona(this.classPersona.idPersona!, this.classPersona)
-			.subscribe(
-				(data) => {
-					if (data != null) {
-						this.persona = { ...this.classPersona };
-
-						//this.toastrService.success('Actualización exitosa', '¡Bien hecho!');
-
-						this.usuarioService
-							.updateUsuario(this.classUsuario.idUsuario!, this.classUsuario)
-							.subscribe((data1) => {
-								if (data1 != null) {
-									this.toastrService.success(
-										'Actualización exitosa de los datos',
-										'¡Bien hecho!'
-									);
-
-									this.persona = { ...this.classPersona };
-
-									if (this.usuLoginRol === 'DocenteCapacitador') {
-										this.capacitadorService
-											.updateCapacitador(
-												this.classCapacitador.idCapacitador!,
-												this.classCapacitador
-											)
-											.subscribe((data2) => {
-												if (data2 != null) {
-													this.persona = { ...this.classPersona };
-
-													this.toastrService.success(
-														'Actualización exitosa de Docente Capacitador',
-														'¡Bien hecho!'
-													);
-												}
-											});
-									} else {
-										setTimeout(() => {
-											localStorage.removeItem('emp')
-											localStorage.setItem('emp', String('CHECK'));
-											location.reload();
-										}, 1000);
-										return;
-									}
-
-									setTimeout(() => {
-										localStorage.removeItem('emp')
-										localStorage.setItem('emp', String('CHECK'));
-										location.reload();
-									}, 1000);
-								}
-							});
-
-						//Implementacion de la carga
-					}
-				},
-				(error) => {
-					console.error(error);
-					this.toastrService.error(
-						'Error al actualizar los datos',
-						'Por favor intenta más tarde'
-					);
-				}
-			);
-	}
-
-
-	public editPersona() {
-		if (this.usuLoginRol === 'DocenteCapacitador') {
-			if (
-				!this.classPersona.identificacion ||
-				!this.classPersona.nombre1 ||
-				!this.classPersona.nombre2 ||
-				!this.classPersona.apellido1 ||
-				!this.classPersona.apellido2 ||
-				!this.classPersona.fechaNacimiento ||
-				!this.classPersona.direccion ||
-				!this.classPersona.telefono ||
-				!this.classPersona.celular ||
-				!this.classPersona.correo ||
-				!this.classPersona.genero ||
-				!this.classPersona.etnia ||
-				!this.classPersona.nivelInstruccion ||
-				!this.classCapacitador.tituloCapacitador ||
-				!this.classCapacitador.tipoAbreviaturaTitulo
-			) {
-				this.toastrService.warning(
-					'Uno o más campos vacíos',
-					'Por favor complete todos los campos'
-				);
-				return;
-			}
-		} else {
-			if (
-				!this.classPersona.identificacion ||
-				!this.classPersona.nombre1 ||
-				!this.classPersona.nombre2 ||
-				!this.classPersona.apellido1 ||
-				!this.classPersona.apellido2 ||
-				!this.classPersona.fechaNacimiento ||
-				!this.classPersona.direccion ||
-				!this.classPersona.telefono ||
-				!this.classPersona.celular ||
-				!this.classPersona.correo ||
-				!this.classPersona.genero ||
-				!this.classPersona.etnia ||
-				!this.classPersona.nivelInstruccion
-			) {
-				this.toastrService.warning(
-					'Uno o más campos vacíos',
-					'Por favor complete todos los campos'
-				);
-				return;
-			}
-		}
-
-		// Verificar si hay campos vacíos
-
-		// Realizar actualización
-		this.personaService
-			.updatePersona(this.classPersona.idPersona!, this.classPersona)
-			.subscribe(
-				(data) => {
-					if (data != null) {
-						this.persona = { ...this.classPersona };
-
-						//this.toastrService.success('Actualización exitosa', '¡Bien hecho!');
-
-						this.usuarioService
-							.updateUsuario(this.classUsuario.idUsuario!, this.classUsuario)
-							.subscribe((data1) => {
-								if (data1 != null) {
-									this.toastrService.success(
-										'Actualización exitosa de los datos',
-										'¡Bien hecho!'
-									);
-
-									this.persona = { ...this.classPersona };
-
-									if (this.usuLoginRol === 'DocenteCapacitador') {
-										this.capacitadorService
-											.updateCapacitador(
-												this.classCapacitador.idCapacitador!,
-												this.classCapacitador
-											)
-											.subscribe((data2) => {
-												if (data2 != null) {
-													this.persona = { ...this.classPersona };
-
-													this.toastrService.success(
-														'Actualización exitosa de Docente Capacitador',
-														'¡Bien hecho!'
-													);
-												}
-											});
-									} else {
-										setTimeout(() => {
-											localStorage.removeItem('emp')
-											localStorage.setItem('emp', String('CHECK'));
-											location.reload();
-										}, 1000);
-										return;
-									}
-
-									setTimeout(() => {
-										localStorage.removeItem('emp')
-										localStorage.setItem('emp', String('CHECK'));
-										location.reload();
-									}, 1000);
-								}
-							});
-
-						//Implementacion de la carga
-					}
-				},
-				(error) => {
-					console.error(error);
-					this.toastrService.error(
-						'Error al actualizar los datos',
-						'Por favor intenta más tarde'
-					);
-				}
-			);
-	}
-
 
 
 	selectedFile!: File;
