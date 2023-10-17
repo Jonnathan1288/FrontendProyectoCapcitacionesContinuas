a065ea820db67fbfab8e50cf9288fa43f61ecc6b
Diana Zhizhin <dzhizhin632@gmail.com>
2023-10-17 12:14:30 -0500

    MODEL-SERVICE

---
+++ src/app/models/references/notasFinales-filter.ts
@@ -0,0 +1,18 @@
+export class NotasFinalesFilter{
+
+    idNota?:number
+    informe1?:number
+    informe2?:number
+    informe3?:number
+    examenFinal?:number
+    observacion?: string
+    identificacion?: string
+    idCurso?: number
+    nombreCurso?: string
+    fechaInicioCurso?: Date;
+    fechaFinCurso?: Date;
+    idParticipanteMatriculado?: number
+
+
+
+}
